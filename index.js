4979f9629b9fd298b1e2a806707e16859fbf9a16
bryanquezada-sketch <bryan.quezada@careerpathonline.org>
2026-02-02 18:45:15 -0500

    Revert "Revert "feature/Duck Quotes. Actual insight now possible.""

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import BBCodeTextPlugin from '/node_modules/phaser3-rex-plugins/plugins/bbcodetext-plugin.js';
+
 import MainScene from "./scenes/MainScene.js";
 import UIScene from "./scenes/UIScene.js";
 
@@ -7,6 +9,13 @@ window.PHSR_GAME = new Phaser.Game({
   width: 150,
   height: 150,
   parent: "game-container",
+  plugins: {
+    global: [{
+      key: 'rexBBCodeTextPlugin',
+      plugin: BBCodeTextPlugin,
+      start: true
+    }]
+  },
   scene: [MainScene, UIScene],
   physics: {
     default: "arcade",
